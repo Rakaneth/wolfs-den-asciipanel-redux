8c034a31839f51b559e8a2c0ba7905945f8b6471
Rakaneth <rakaneth@gmail.com>
2019-03-08 18:05:54 -0500

    map drawing almost there

--- src/main/java/com/rakaneth/wolfsden/GameContext.java
+++ src/main/java/com/rakaneth/wolfsden/GameContext.java
@@ -2,10 +2,13 @@ package com.rakaneth.wolfsden;
 
 import com.rakaneth.wolfsden.entity.GameObject;
 import com.rakaneth.wolfsden.map.GameMap;
+import squidpony.squidmath.Coord;
 import squidpony.squidmath.RNG;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GameContext {
     private static GameContext instance;
@@ -32,7 +35,7 @@ public class GameContext {
     }
 
     public void addMap(GameMap m) {
-        maps.put(m.getName(), m);
+        maps.put(m.getID(), m);
     }
 
     public void addEntity(GameObject o) {
@@ -43,9 +46,37 @@ public class GameContext {
         things.remove(o.getID());
     }
 
+    public GameObject getEntity(String eID) {
+        return things.get(eID);
+    }
+
     public GameMap curMap() {
         return getMap(curMapID);
     }
 
+    public GameObject player() {
+        return getEntity("player");
+    }
+
+    public boolean isHere(GameObject o) {
+        return o.getMapID()
+                .equals(curMapID);
+    }
+
+    public List<GameObject> thingsAt(Coord c) {
+        return things.values()
+                     .stream()
+                     .filter(el -> el.getPos() == c && isHere(el))
+                     .collect(Collectors.toList());
+    }
+
+    public void changeMap(GameMap.MapConnection conn) {
+        curMapID = conn.toMapID;
+        player().move(conn.toPt);
+    }
+
+    public void changeMap(String mapID) {
+        curMapID = mapID;
+    }
 
 }
